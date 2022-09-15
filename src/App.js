cf18366f483fcbb80a51f544dfd2c2180ab7fc21
Phan Quang <quangphan0724@gmail.com>
2022-09-15 11:59:49 +0700

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Body from './Components/BTThucHanhLayOut/Body/Body';
+import Header from './Components/BTThucHanhLayOut/Header/Header';
+import Footer from './Components/BTThucHanhLayOut/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
